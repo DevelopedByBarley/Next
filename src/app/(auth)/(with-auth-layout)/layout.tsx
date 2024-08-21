dbd2332a8ac6166efa2823cc943f623c23d1c178
Szaniszló Árpád (MArpadszF) <developedbybarley@gmail.com>
2024-08-21 14:35:17 +0200

    Ep 20 -> Active links

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,14 +1,34 @@
+'use client';
+import './style.css'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" }
+]
 
-export default function RootLayout({
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pathName = usePathname();
   return (
     <>
-        {children}
-      <h1>This is a nested route</h1>
+      {
+        navLinks.map((link) => {
+          const isActive = pathName.startsWith(link.href);          
+          return (
+            <Link href={link.href} key={link.name} className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}>
+              {link.name}
+            </Link>
+          )
+        })
+      }
+
+      {children}
     </>
   )
 }
