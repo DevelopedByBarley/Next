2113e177bdfaa190b5f1cb8db562551dc5019617
Szaniszló Árpád (MArpadszF) <developedbybarley@gmail.com>
2024-08-21 14:20:41 +0200

    Ep 19 -> Link Component navigation

---
+++ src/app/products/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next"; // Ensure this import is correct and used properly
+import Link from "next/link";
+
+
+
+export default function Products() {
+    return (
+        <>
+            <Link href='/products/1'>Product 1</Link>
+            <Link href='/products/2'>Product 2</Link>
+            <Link href="/products/3" replace>Product 3</Link> // Replace 
+            
+
+        </>
+    )
+}
