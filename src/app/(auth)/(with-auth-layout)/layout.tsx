e948915e3ec6f130daa5ffeb6c8ce91636e31d49
Szaniszló Árpád (MArpadszF) <developedbybarley@gmail.com>
2024-08-26 14:06:53 +0200

    Ep 22 - Templates

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -2,6 +2,7 @@
 import './style.css'
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useState } from 'react';
 
 const navLinks = [
   { name: "Register", href: "/register" },
@@ -14,12 +15,18 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [input, setInput] = useState("");
   const pathName = usePathname();
+
+
   return (
     <>
+      <div>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className='border p-2'/>
+      </div>
       {
         navLinks.map((link) => {
-          const isActive = pathName.startsWith(link.href);          
+          const isActive = pathName.startsWith(link.href);
           return (
             <Link href={link.href} key={link.name} className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}>
               {link.name}
@@ -32,3 +39,25 @@ export default function AuthLayout({
     </>
   )
 }
+
+
+/**
+ * 
+ * 
+ * A példában, amit megosztottál, a useState hookot használod egy input értékének nyomon követésére. Amikor a 
+ * layout.tsx fájlt használod a navigáció során, a state megmarad, de amikor a template.tsx fájlon keresztül navigálsz, 
+ * a state elveszik. Ez az eltérés abból adódik, ahogyan 
+ * a layout és a template komponensek kezelik az állapotot és a komponensek újrarenderelését.
+
+Miért marad meg a state a layout.tsx használatával?
+A layout.tsx fájlok perzisztens komponensek, ami azt jelenti, hogy amikor navigálsz 
+az oldalak között az adott layout alatt, akkor a layout komponens újra felhasználásra kerül,
+ és nem renderelődik újra teljesen. Ezért a state, amit a layout.tsx-ben definiálsz, megmarad, 
+ mert a komponens nem kerül újralétrehozásra, csak az alatta lévő gyermek komponensek változnak.
+
+Miért veszik el a state a template.tsx használatával?
+A template.tsx fájlok dinamikus komponensek, amelyeket minden alkalommal újrarenderel a Next.js,
+ amikor navigálsz az útvonalak között, amelyekhez a template tartozik. Ez azt jelenti, hogy amikor a
+  template.tsx-en keresztül navigálsz, a komponens újra inicializálódik,
+   és ezzel együtt minden state is újrainicializálódik, így elveszik az előző állapot.
+ */
